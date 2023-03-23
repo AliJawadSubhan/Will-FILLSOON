15374f4da5439510f90c2eb3cfd332cc1f9978cc
Ali Jawad <jawadsubhan7@gmail.com>
2023-03-24 00:07:33 +0500

    experimented but failed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,9 +9,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Color? pickedColor;
-  void pickColor(Color colour) {
+  pickColor(int colorID) {
+    var colour = myColors[colorID];
+
     setState(() {
       if (colour != pickedColor) {
+        colorsW[colorID].isVisible = true;
         pickedColor = colour;
       } else {
         pickedColor = Colors.white;
@@ -19,7 +22,30 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  List<Color> myColors = [Colors.black, Colors.orange];
+  List<Color> myColors = [
+    Colors.black,
+    Colors.orange,
+    Colors.red,
+    Colors.blue,
+    Colors.blueAccent,
+    Colors.redAccent,
+    Colors.purple,
+    Colors.purpleAccent,
+    Colors.grey,
+  ];
+
+  bool checkVisivility(int colorID) {
+    for (var i = 0; i < myColors.length; i++) {
+      return colorID == i;
+    }
+
+    return false;
+  }
+
+  bool check = false;
+
+  List<ColorPicker> colorsW = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,27 +63,67 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Text('Get your color now'),
               ),
             ),
-            GestureDetector(
-              onTap: () {
-                pickColor(myColors[0]);
-              },
-              child: Container(
-                color: Colors.black,
-                height: 20,
-                width: 50,
+            SizedBox(
+              height: 400,
+              child: GridView.builder(
+                // physics: NeverScrollableScrollPhysics(),
+                itemCount: myColors.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemBuilder: (_, idx) {
+                  var colorPicker = ColorPicker(
+                      isVisible: colorsW[idx].isVisible,
+                      color: myColors[idx],
+                      onTap: () {
+                        checkVisivility(idx);
+                        pickColor(idx);
+                        print("Picking Color");
+                      });
+
+                  colorsW.add(colorPicker);
+                  return colorPicker;
+                },
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                pickColor(myColors[1]);
-              },
-              child: Container(
-                color: Colors.orange,
-                height: 20,
-                width: 50,
-              ),
-            ),
+            )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ColorPicker extends StatelessWidget {
+  ColorPicker(
+      {super.key, this.color, required this.onTap, required this.isVisible});
+
+  final Color? color;
+  final VoidCallback onTap;
+  var isVisible = false;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: color ?? Colors.lightBlue,
+            shape: BoxShape.circle,
+          ),
+          height: 20,
+          width: 50,
+          child: Visibility(
+            visible: isVisible,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Icon(
+                  Icons.check,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
